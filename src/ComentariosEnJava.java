5e4d1542bce66c8c9d27f5c4e1fe6205a2822865
leila valencia <levamo26@outlook.com>
2022-08-08 19:56:37 -0500

    primeros cambios

---
+++ src/ComentariosEnJava.java
@@ -0,0 +1,12 @@
+public class ComentariosEnJava {
+    public static void main (String [] args){
+        System.out.println("curso java");
+        //esta es una linea
+
+        /*
+        Hola que tal
+        Mi nombre es Leila
+        Curso java
+         */
+    }
+}
---
+++ src/TiposDeDatosPrimitivos.java
@@ -0,0 +1,7 @@
+public class TiposDeDatosPrimitivos {
+    public static void main (String[]args){
+        short entero=11540;
+        System.out.println("numero entero"+entero);
+
+    }
+}
